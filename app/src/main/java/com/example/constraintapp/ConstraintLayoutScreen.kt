0ab26c41cf224ce0dd5286cca777d2d36e33140e
beewoo01 <beewoo01@gmail.com>
2026-06-18 12:40:35 +0900

    section 11: 83.Chains

--- app/src/main/java/com/example/constraintapp/ConstraintLayoutScreen.kt
+++ app/src/main/java/com/example/constraintapp/ConstraintLayoutScreen.kt
@@ -2,6 +2,7 @@ package com.example.constraintapp
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
@@ -9,16 +10,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
 
 @Composable
 fun ConstraintLayoutScreen() {
-    ConstraintLayout(modifier = Modifier.fillMaxWidth()) {
+    ConstraintLayout(modifier = Modifier.fillMaxSize()) {
         // 1- Define the
         val (box1, box2, text) = createRefs()
 
 
-        val guideLine1 = createGuidelineFromStart(0.15f)
+        val guideLine1 = createGuidelineFromStart(0.05f)
+
+        val barrier1 = createEndBarrier(box1, box2)
 
 
         Box(
@@ -27,14 +31,14 @@ fun ConstraintLayoutScreen() {
                 .background(Color.Red)
                 .constrainAs(box1) {
                     top.linkTo(parent.top, margin = 100.dp)
-                    start.linkTo(guideLine1, margin = 40.dp)
+                    start.linkTo(guideLine1)
 
                 }
         )
 
         Box(
             modifier = Modifier
-                .size(250.dp)
+                .size(80.dp)
                 .background(Color.Green)
                 .constrainAs(box2) {
                     top.linkTo(box1.bottom, margin = 20.dp)
@@ -46,8 +50,13 @@ fun ConstraintLayoutScreen() {
             text = "Hello Constraint Layout",
             modifier = Modifier.constrainAs(text) {
                 top.linkTo(box2.bottom, margin = 16.dp)
-                start.linkTo(guideLine1)
+                start.linkTo(barrier1, margin = 16.dp)
             })
+
+        createEndBarrier(box1, box2)
+
+
+        createVerticalChain(box1, box2, chainStyle = ChainStyle.Spread)
 
     }
 }
